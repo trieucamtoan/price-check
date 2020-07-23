07141ac4a749bd3a4b4d83a320da5897e1f5b911
nbagheri <negar_bagheri_hariri@sfu.ca>
2020-07-23 16:53:20 -0700

    Product Model

---
+++ backend/api/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Product(models.Model):
+    name       = models.CharField(max_length=120) # max_length = required
+    description = models.TextField(blank=True, null=True)
+    URL = models.URLField(max_length = 200)
+    current_price = models.DecimalField(decimal_places = 2,max_digits = 1000,null = True)
+    lowest_price  = models.DecimalField( decimal_places = 2,max_digits = 1000,null = True)   
