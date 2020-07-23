51b3f760aad4a9b19ff0a21997a236044e7367be
ctrieu <ctrieu@sfu.ca>
2020-07-23 15:04:17 -0700

    Add clear localStorage token

--- frontend/src/components/user/Logout.js
+++ frontend/src/components/user/Logout.js
@@ -8,10 +8,12 @@ function Logout(props) {
     const logout = () => {
         if (localStorage.getItem('isLoggedIn') === 'true') {
             localStorage.setItem("isLoggedIn", "false")
+            localStorage.setItem("token", "")
             props.history.push('/')
             window.location.reload()
         } else {
             props.history.push('/')
+            localStorage.setItem("token", "")
             window.location.reload()
         }
     }
